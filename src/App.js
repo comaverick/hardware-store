9e72be92f0321f9b9f44a93860ba74096fce2ae3
comaverick <maverickhalili@gmail.com>
2026-08-22 10:52:52 +0800

    fix when first loaded. sidebar topbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthContext";
 
@@ -205,13 +205,7 @@ function App() {
           />
           <Route
             path="*"
-            element={
-              <ProtectedRoute>
-                <AppLayout>
-                  <Dashboard />
-                </AppLayout>
-              </ProtectedRoute>
-            }
+            element={<Navigate to="/dashboard" replace />}
           />
         </Routes>
       </AuthProvider>
